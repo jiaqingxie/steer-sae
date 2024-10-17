92473edb978878f45eab8b763e1b7ab58573c177
JiaX-TCS <jiaxie@student.ethz.ch>
2024-10-17 10:28:42 +0200

    new

---
+++ train_sh/sae_gsm8k.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/%j.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/%j.err
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=rtx_3090:1
+#SBATCH --time=3:00:00
+
+module load eth_proxy
+#export TRANSFORMERS_CACHE=/cluster/scratch/jiaxie/.cache
+export TRITON_CACHE_DIR=/cluster/scratch/jiaxie/.triton_cache
+
+
+cd /cluster/scratch/jiaxie/
+source sae/bin/activate
+
+cd /cluster/project/sachan/jiaxie/SAE_Math
+
+#python gsm8k/vllm_main.py --model_name_or_path=google/gemma-2-9b-it --cache_dir=/cluster/scratch/jiaxie/models/google/gemma-2-9b-it
+#python gsm8k/vllm_main.py --model_name_or_path=google/gemma-2-9b --cache_dir=/cluster/scratch/jiaxie/models/google/gemma-2-9b
+
+
+
+#Settings
+MODEL_NAME_OR_PATH="google/gemma-2-2b"
+DEBUG=False
+CACHE_DIR="/cluster/scratch/jiaxie/models/google/gemma-2-2b"
+COT_FLAG=False
+LAYER_IDX=20
+K=5
+TYPE="sae"
+SAE_FILE="google/gemma-scope-2b-pt-res"
+PARAM_FILE="layer_20/width_16k/average_l0_71/params.npz"
+VLLM=False
+TRANSFORMER_LENS=True
+
+
+
+python -u gsm8k/sae.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --debug ${DEBUG} \
+    --cache_dir ${CACHE_DIR} \
+    --cot_flag ${COT_FLAG} \
+    --layer_idx ${LAYER_IDX} \
+    --K ${K} \
+    --type ${TYPE} \
+    --sae_file ${SAE_FILE} \
+    --param_file ${PARAM_FILE} \
+    --vllm ${VLLM} \
+    --transformer_lens ${TRANSFORMER_LENS} \
