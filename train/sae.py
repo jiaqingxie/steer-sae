4e237a0410aeac3c12128eeb0291e8e7a2818d9a
JiaX-TCS <jiaxie@student.ethz.ch>
2024-10-23 10:34:13 +0200

    add additional prompt file to import

--- train/sae.py
+++ train/sae.py
@@ -14,6 +14,7 @@ import transformer_lens
 from huggingface_hub import hf_hub_download
 from models.SAE.JumpReLU import JumpReLUSAE
 import gc
+from prompts.prompts import get_examples
 
 
 
@@ -51,93 +52,13 @@ def is_correct(model_answer, answer):
         return model_answer == gt_answer or float(model_answer) == float(gt_answer)
 
 
-def create_demo_text(n_shot=8, cot_flag=True):
+def create_demo_text(n_shot=8, cot_flag=True, dataset):
+    examples = get_examples()
     question, chain, answer = [], [], []
-    question.append(
-        "There are 15 trees in the grove. "
-        "Grove workers will plant trees in the grove today. "
-        "After they are done, there will be 21 trees. "
-        "How many trees did the grove workers plant today?"
-    )
-    chain.append(
-        "There are 15 trees originally. "
-        "Then there were 21 trees after some more were planted. "
-        "So there must have been 21 - 15 = 6."
-    )
-    answer.append("6")
-
-    question.append(
-        "If there are 3 cars in the parking lot and 2 more cars arrive, "
-        "how many cars are in the parking lot?"
-    )
-    chain.append("There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5.")
-    answer.append("5")
-
-    question.append(
-        "Leah had 32 chocolates and her sister had 42. If they ate 35, "
-        "how many pieces do they have left in total?"
-    )
-    chain.append(
-        "Originally, Leah had 32 chocolates. "
-        "Her sister had 42. So in total they had 32 + 42 = 74. "
-        "After eating 35, they had 74 - 35 = 39."
-    )
-    answer.append("39")
-
-    question.append(
-        "Jason had 20 lollipops. He gave Denny some lollipops. Now Jason "
-        "has 12 lollipops. How many lollipops did Jason give to Denny?"
-    )
-    chain.append(
-        "Jason started with 20 lollipops. Then he had 12 after giving some "
-        "to Denny. So he gave Denny 20 - 12 = 8."
-    )
-    answer.append("8")
-
-    question.append(
-        "Shawn has five toys. For Christmas, he got two toys each from his "
-        "mom and dad. How many toys does he have now?"
-    )
-    chain.append(
-        "Shawn started with 5 toys. If he got 2 toys each from his mom and "
-        "dad, then that is 4 more toys. 5 + 4 = 9."
-    )
-    answer.append("9")
-
-    question.append(
-        "There were nine computers in the server room. Five more computers "
-        "were installed each day, from Monday to Thursday. "
-        "How many computers are now in the server room?"
-    )
-    chain.append(
-        "There were originally 9 computers. For each of 4 days, 5 more "
-        "computers were added. So 5 * 4 = 20 computers were added. "
-        "9 + 20 is 29."
-    )
-    answer.append("29")
-
-    question.append(
-        "Michael had 58 golf balls. On Tuesday, he lost 23 golf balls. On "
-        "Wednesday, he lost 2 more. "
-        "How many golf balls did he have at the end of Wednesday?"
-    )
-    chain.append(
-        "Michael started with 58 golf balls. After losing 23 on Tuesday, "
-        "he had 58 - 23 = 35. After losing 2 more, "
-        "he had 35 - 2 = 33 golf balls."
-    )
-    answer.append("33")
-
-    question.append(
-        "Olivia has $23. She bought five bagels for $3 each. "
-        "How much money does she have left?"
-    )
-    chain.append(
-        "Olivia had 23 dollars. "
-        "5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. "
-        "So she has 23 - 15 dollars left. 23 - 15 is 8."
-    )
-    answer.append("8")
+    for q, c, a in examples[dataset]:
+        question.append(q)
+        chain.append(c)
+        answer.append(a)
 
     # randomize order of the examples ...
     index_list = list(range(len(question)))
@@ -171,8 +92,8 @@ def create_demo_text(n_shot=8, cot_flag=True):
     return demo_text
 
 
-def build_prompt(input_text, n_shot, cot_flag):
-    demo = create_demo_text(n_shot, cot_flag)
+def build_prompt(input_text, n_shot, cot_flag, dataset):
+    demo = create_demo_text(n_shot, cot_flag, dataset)
     if cot_flag:
         input_text_prompt = demo + "Q: " + input_text + "\n Please reason step by step. " + "A:"
 
@@ -335,6 +256,12 @@ def parse_args():
         help="parameter file name on huggingface"
     )
     parser.add_argument(
+        "--dataset",
+        type=str,
+        default="gsm8k",
+        help="Evaluation Dataset Name"
+    )
+    parser.add_argument(
         "--plot_num",
         type=int,
         default=15,
@@ -408,7 +335,7 @@ def main():
 
     answers = [] if args.type == "inference" else {}
     for sample in tqdm(list_data_dict):
-        input_text = build_prompt(sample["instruction"], N_SHOT, args.cot_flag)
+        input_text = build_prompt(sample["instruction"], N_SHOT, args.cot_flag, args.dataset)
         input_text = input_text.strip(" ")
 
         if args.vllm:
