0e483ecd282a209aea43f98f06fc5135d3ded549
JiaX-TCS <jiaxie@student.ethz.ch>
2024-12-07 21:34:38 +0100

    add 9b sae

--- train_sh/gsm8k/sae_gsm8k_9b_COT.sh
+++ train_sh/gsm8k/sae_gsm8k_9b_COT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --output=/cluster/project/sachan/jiaxie/results/9b_cumulative_cot.out
-#SBATCH --error=/cluster/project/sachan/jiaxie/results/9b_cumulative_cot.err
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/9b_cumulative_no_cot.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/9b_cumulative_no_cot.err
 #SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=rtx_3090:2
@@ -45,9 +45,9 @@ python -u train/sae.py \
     --sae_file ${SAE_FILE} \
     --param_file ${PARAM_FILE} \
     --transformer_lens \
-    --cot_flag \
     --sae_id ${SAE_ID} \
     --devices ${N_DEVICES} \
     --dataset ${DATASET} \
     --bfloat16 \
     --NUM_SAE ${NUM_SAE} \
+    --cumulative
