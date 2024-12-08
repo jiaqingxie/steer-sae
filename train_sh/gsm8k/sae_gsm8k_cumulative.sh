16c7d6ce5a88b070967f4e3ff2a93d0bf3a4aa6a
JiaX-TCS <jiaxie@student.ethz.ch>
2024-12-08 03:47:33 +0100

    Update sae_gsm8k_cumulative.sh

--- train_sh/gsm8k/sae_gsm8k_cumulative.sh
+++ train_sh/gsm8k/sae_gsm8k_cumulative.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --output=/cluster/project/sachan/jiaxie/results/2b_cumulative_no_cot.out
-#SBATCH --error=/cluster/project/sachan/jiaxie/results/2b_cumulative_no_cot.err
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/2b_cumulative_cot.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/2b_cumulative_cot.err
 #SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=rtx_3090:1
@@ -42,7 +42,7 @@ python -u train/sae.py \
     --sae_file ${SAE_FILE} \
     --transformer_lens \
     --sae_id ${SAE_ID} \
-    --seed ${SEED} \
     --cumulative \
     --dataset ${DATASET} \
     --NUM_SAE ${NUM_SAE} \
+    --cot_flag \
