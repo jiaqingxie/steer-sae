0cd58312c590781645490dd3bc89d0f6b18da159
JiaX-TCS <jiaxie@student.ethz.ch>
2024-12-08 03:15:56 +0100

    Update sae_gsm8k_cumulative.sh

--- train_sh/gsm8k/sae_gsm8k_cumulative.sh
+++ train_sh/gsm8k/sae_gsm8k_cumulative.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --output=/cluster/project/sachan/jiaxie/results/%j.out
-#SBATCH --error=/cluster/project/sachan/jiaxie/results/%j.err
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/2b_cumulative_no_cot.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/2b_cumulative_no_cot.err
 #SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=rtx_3090:1
@@ -11,7 +11,6 @@ module load eth_proxy
 export TRANSFORMERS_CACHE=/cluster/scratch/jiaxie/.cache
 export TRITON_CACHE_DIR=/cluster/scratch/jiaxie/.triton_cache
 
-#export TRANSFORMERS_OFFLINE=1
 cd /cluster/scratch/jiaxie/
 source sae/bin/activate
 
@@ -22,7 +21,7 @@ cd /cluster/project/sachan/jiaxie/SAE_Math
 MODEL_NAME_OR_PATH="google/gemma-2-2b"
 DATA_ROOT="/cluster/project/sachan/jiaxie/SAE_Math/data"
 CACHE_DIR="/cluster/scratch/jiaxie/models/google/gemma-2-2b"
-LAYER_IDX=21
+LAYER_IDX=20
 PLOT_NUM=5
 K=10
 TYPE="sae"
@@ -30,7 +29,7 @@ SAE_FILE="gemma-scope-2b-pt-res-canonical"
 SAE_ID="21-gemmascope-res-16k"
 TRANSFORMER_LENS=True
 DATASET="gsm8k_train"
-SEED=0
+NUM_SAE=1000
 
 python -u train/sae.py \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
@@ -41,9 +40,10 @@ python -u train/sae.py \
     --K ${K} \
     --type ${TYPE} \
     --sae_file ${SAE_FILE} \
+    --param_file ${PARAM_FILE} \
     --transformer_lens \
     --sae_id ${SAE_ID} \
-    --cot_flag \
     --seed ${SEED} \
     --cumulative \
     --dataset ${DATASET} \
+    --NUM_SAE ${NUM_SAE} \
