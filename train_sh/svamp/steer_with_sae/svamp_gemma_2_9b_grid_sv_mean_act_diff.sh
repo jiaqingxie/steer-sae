06c67284c3e32d8795d9602fdf44a377ac679e77
JiaX-TCS <jiaxie@student.ethz.ch>
2024-12-15 05:55:46 +0800

    new

--- train_sh/svamp/steer_with_sae/svamp_gemma_2_9b_grid_sv_mean_act_diff.sh
+++ train_sh/svamp/steer_with_sae/svamp_gemma_2_9b_grid_sv_mean_act_diff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --output=/cluster/project/sachan/jiaxie/results/sae_9b_svamp_0shot_C200_T1_omega1.out
-#SBATCH --error=/cluster/project/sachan/jiaxie/results/sae_9b_svamp_0shot_C200_T1_omega1.err
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/sae_svamp_0shot_C15_T3_omega1_mean_act_vec.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/sae_svamp_0shot_C15_T3_omega1_mean_act_vec.err
 #SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=rtx_3090:2
@@ -15,16 +15,16 @@ export TRITON_CACHE_DIR=/cluster/scratch/jiaxie/.triton_cache
 cd /cluster/scratch/jiaxie/
 source sae/bin/activate
 
-cd /cluster/project/sachan/jiaxie/SAE_Math""
+cd /cluster/project/sachan/jiaxie/SAE_Math
 
 #Settings alphabetically
 CACHE_DIR="/cluster/scratch/jiaxie/models/google/gemma-2-9b"
-COEFF=(200 900)
+COEFF=(15 900)
 DATA_ROOT="/cluster/project/sachan/jiaxie/SAE_Math/data"
 K=10
 LAYER_IDX=31
 MODEL_NAME_OR_PATH="google/gemma-2-9b"
-PARAM_FILE="layer_31/width_16k/average_l0_63/params.npz"
+PARAM_FILE="layer_31/width_16k/average_l0_71/params.npz"
 PLOT_NUM=5
 SAE_FILE="gemma-scope-9b-pt-res-canonical"
 SAE_ID="31-gemmascope-res-16k"
@@ -32,9 +32,12 @@ SAE_IDX=(12946)
 TRANSFORMER_LENS=True
 TYPE="inference"
 N_SHOT=0
-T=1
+T=3
 OMEGA=1
+STEERING_TYPE="mean_act_diff"
+steer_vec_base_directory="/cluster/project/sachan/jiaxie/SAE_Math/mean_vec"
 DATASET="svamp"
+N_DEVICES=2
 
 python -u train/sae.py \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
@@ -54,6 +57,9 @@ python -u train/sae.py \
     --sae_idx ${SAE_IDX[@]} \
     --coeff ${COEFF[@]} \
     --T ${T} \
-    --bfloat16 \
     --omega ${OMEGA} \
-    --dataset  ${DATASET} \
+    --steering_type ${STEERING_TYPE} \
+    --steer_vec_base_directory ${steer_vec_base_directory} \
+    --dataset ${DATASET} \
+    --bfloat16 \
+    --devices ${N_DEVICES} \
