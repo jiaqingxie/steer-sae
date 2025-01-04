e5be21dc2a0229de2d901f968f64baa9b912ff38
JiaX-TCS <jiaxie@student.ethz.ch>
2025-01-04 19:31:24 +0800

    change from "To" to "to"

---
+++ train_sh/mawps/sae_mawps_9b_inference_to.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --output=/cluster/project/sachan/jiaxie/results/sae_9b_mawps_inference_to.out
+#SBATCH --error=/cluster/project/sachan/jiaxie/results/sae_9b_mawps_inference_to.err
+#SBATCH --mem-per-cpu=20G
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=rtx_3090:1
+#SBATCH --time=3:00:00
+
+module load eth_proxy
+export HF_HOME=/cluster/scratch/jiaxie/.cache/huggingface
+export TRANSFORMERS_CACHE=/cluster/scratch/jiaxie/.cache
+export TRITON_CACHE_DIR=/cluster/scratch/jiaxie/.triton_cache
+
+cd /cluster/scratch/jiaxie/
+source sae/bin/activate
+
+cd /cluster/project/sachan/jiaxie/SAE_Math
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+#Settings
+MODEL_NAME_OR_PATH="google/gemma-2-9b"
+DATA_ROOT="/cluster/project/sachan/jiaxie/SAE_Math/data"
+CACHE_DIR="/cluster/scratch/jiaxie/models/google/gemma-2-9b"
+LAYER_IDX=31
+PLOT_NUM=5
+K=10
+TYPE="inference"
+SAE_FILE="gemma-scope-9b-pt-res-canonical"
+SAE_ID="31-gemmascope-res-16k"
+PARAM_FILE="layer_31/width_16k/average_l0_63/params.npz"
+N_SHOTS=0
+DATASET="mawps"
+SAE_WORD="to"
+
+python -u train/sae.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --data_root ${DATA_ROOT} \
+    --cache_dir ${CACHE_DIR} \
+    --layer_idx ${LAYER_IDX} \
+    --plot_num ${PLOT_NUM} \
+    --K ${K} \
+    --type ${TYPE} \
+    --sae_file ${SAE_FILE} \
+    --param_file ${PARAM_FILE} \
+    --sae_id ${SAE_ID} \
+    --n_shot ${N_SHOTS} \
+    --vllm \
+    --bfloat16 \
+    --dataset ${DATASET} \
+    --sae_word ${SAE_WORD} \
